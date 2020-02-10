8f660fdfd422ad6357220824aaadf96a3ce1b2a9
nyceane <nyceane@gmail.com>
2020-02-10 03:12:38 -0800

    added image convert and image test

--- posenet/utils.py
+++ posenet/utils.py
@@ -99,3 +99,28 @@ def draw_skel_and_kp(
         flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     out_img = cv2.polylines(out_img, adjacent_keypoints, isClosed=False, color=(255, 255, 0))
     return out_img
+
+def draw_skel_and_kp_figureonly(
+        img, instance_scores, keypoint_scores, keypoint_coords,
+        min_pose_score=0.5, min_part_score=0.5):
+    out_img = np.zeros((img.shape[0],img.shape[1] ,img.shape[2]), np.uint8)
+    adjacent_keypoints = []
+    cv_keypoints = []
+    for ii, score in enumerate(instance_scores):
+        if score < min_pose_score:
+            continue
+
+        new_keypoints = get_adjacent_keypoints(
+            keypoint_scores[ii, :], keypoint_coords[ii, :, :], min_part_score)
+        adjacent_keypoints.extend(new_keypoints)
+
+        for ks, kc in zip(keypoint_scores[ii, :], keypoint_coords[ii, :, :]):
+            if ks < min_part_score:
+                continue
+            cv_keypoints.append(cv2.KeyPoint(kc[1], kc[0], 10. * ks))
+
+    out_img = cv2.drawKeypoints(
+        out_img, cv_keypoints, outImage=np.array([]), color=(255, 255, 0),
+        flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    out_img = cv2.polylines(out_img, adjacent_keypoints, isClosed=False, color=(255, 255, 0))
+    return out_img
